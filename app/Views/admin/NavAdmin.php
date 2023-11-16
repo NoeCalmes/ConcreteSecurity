ac05130c012fbc8085fa81944389e60077ab274f
IKaoo <68471197+NoeCalmes@users.noreply.github.com>
2023-11-16 15:46:28 +0100

    commit du navbar admin

--- app/Views/admin/NavAdmin.php
+++ app/Views/admin/NavAdmin.php
@@ -1,4 +1,3 @@
-
 <link
     href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap"
     rel="stylesheet">
@@ -8,34 +7,49 @@
 <link rel="icon" type="image/x-icon" href="<?= base_url('public/favicon.ico') ?>">
 <link rel="stylesheet" type="text/css" href="<?= base_url('public/css/StyleAdmin.css') ?>">
 
-
-<img src="<?= base_url('public/img/acceuil/logo.svg') ?>" alt="" class="logo" id="logo">
-
-
-<h1> Bienvenue sur L'admin Panel , (Admin Name) </h1>
-
-<nav class="MainNav">
-    <div>
-        <ul>
-            <li><a href="#" class="navbar">Accueil</a></li>
-
-            <li><a href="#" class="navbar">Contrat</a></li>
-            <!-- <ul>  
-                <li><a href="#" class="navbar">Contrat en Attente</a></li>
-                <li><a href="#" class="navbar">Contrat Actif</a></li>
-                <li><a href="#" class="navbar">Contrat Finis</a></li>
-            </ul>
-           -->
-            <li><a href="#" class="navbar">Demandes</a></li>
-            <li><a href="#" class="navbar">Employés</a></li>
-        </ul>
+<div class="nav-project">
+    <div class="header-nav">
+        <img src="<?= base_url('public/img/acceuil/logo.png') ?>" alt="" class="logo" id="logo">
+        <h1> Bienvenue sur L'admin Panel , (Admin Name) </h1>
     </div>
-</nav>
+
+    <div class="nav">
+        <div class="list-contrat">
+            <a href="#" class="navbar">Accueil</a>
+        </div>
+        <div class="list-contrat">
+            <a href="#" class="navbar">Contrat</a>
+            <div class="arrow"></div>
+        </div>
+        <div class="list-contrat">
+            <a href="#" class="navbar">Demandes</a>
+        </div>
+        <div class="list-contrat">
+            <a href="#" class="navbar">Employés</a>
+        </div>
+    </div>
+
+</div>
+
 
 <style>
 
-    
+
 
 
 
 </style>
+
+<!-- <ul>
+            <li><a href="#" class="navbar">Accueil</a></li>
+
+            <li><a href="#" class="navbar">Contrat</a></li>
+            <ul>  
+                <li><a href="#" class="navbar">Contrat en Attente</a></li>
+                <li><a href="#" class="navbar">Contrat Actif</a></li>
+                <li><a href="#" class="navbar">Contrat Finis</a></li>
+            </ul>
+          
+            <li><a href="#" class="navbar">Demandes</a></li>
+            <li><a href="#" class="navbar">Employés</a></li>
+        </ul> -->
