cb599db6fd456eb5dfe505f76585aa03fb8903de
ultim2503 <raphael.fourquet31@gmail.com>
2023-12-04 15:24:19 +0100

    LastUPD

---
+++ app/Views/admin/MainAdmin.php
@@ -0,0 +1,16 @@
+<body class="paneladmin">
+    <div class="container-acceuil">
+        <div class="Title">
+            <h1>ConcreteSecurity , Acceuil</h1>
+            <div class="border-title"></div>
+        </div>
+
+        <div class="Content">
+            <h2>Concernant L'entreprise</h2>
+            <div class="border-title-content"></div>
+            <div class="Texte">
+                <txt>- Une sortie avec les Employés de l'entreprise sera organisée prochainement afin d'assurer une cohésion d'équipe en cette nouvelle année. L'actvité principale de cette sortie sera le Padel !</txt>
+            </div>
+        </div>
+    </div>
+</body>
