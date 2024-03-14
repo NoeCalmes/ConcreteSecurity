355aa277b2fdbec1e1a3d20a17863da9a4d14bbe
ultim2503 <raphael.fourquet31@gmail.com>
2024-03-14 15:47:56 +0100

    LAST_UPD

--- app/Views/admin/ContratAdmin.php
+++ app/Views/admin/ContratAdmin.php
@@ -5,14 +5,38 @@
             <div class="border-title"></div>
         </div>
 
+        <div class ="Contrat-id-List">
+            <table class =table>
+                <?php
+                    $cpt=0;
+                    foreach($contrats as $c){
+
+                        echo '<td class ="Contrat-Number">';
+                            echo '<b>Contrat n°'. $c->id .'</b>';
+                            echo $logo;
+                        echo '</td>';
+                        echo '<td class ="Contrat-Button">';
+                            echo anchor('admin/contrats/'. $identifiant .'/'. $c->id .'','<i class="fa-solid fa-eye"></i>');
+                        echo '</td>';
+
+
+                        $cpt++;
+
+                        if ($cpt%1==0) {
+                            echo "</tr><tr>";
+                        }   
+                    }
+                    echo '<tr>';
+                    echo'</tr>';
+                ?>
+            </table>
+        </div>
+
         <div class = "liste-contrat">
             <table class =table>
             <?php
-        
-                $cpt=0;
                 echo "<tr>";
-                foreach ($contrats as $contrat) {
-                    echo "<td width='30%' class='text-center'>";
+                    echo "<td width='30%' class='contrat-div'>";
                     echo "<br>";
                     echo " Contrat N° : <b>".$contrat->id."</b>";
                     echo "<br>";
@@ -28,13 +52,15 @@
                     echo "<br>";
                     echo " Adresse: <b>".$contrat->adresse."</b>";
                     echo "<br>";
-                    echo " Periode : <b>".$contrat->cp."</b>";
-                    echo "</td>";
-                    $cpt++;
-                    if ($cpt%1==0) {
-                        echo "</tr><tr>";
+                    if ($contrat->prediode_id == 1){
+                        echo " Periode : <b> Jour </b>";
+                    }else{
+                        echo " Periode : <b> Nuit </b>";
                     }
-                }
+                    echo "<br>";
+                    echo '<div class = "icon">';
+                    echo '</div>';
+                    echo "</td>";
                 echo "</tr>";
             ?>
             </table>
--- app/Views/header.php
+++ app/Views/header.php
@@ -15,4 +15,4 @@
     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
 
 <!-- ICON FONTAWESOME -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+<script src="https://kit.fontawesome.com/6d1f777f80.js" crossorigin="anonymous"></script>
